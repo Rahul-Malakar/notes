9aec5999a1a13d914316dae149cf55bd6b3a58ea
Rahul <malakarahul111@gmail.com>
2023-03-23 10:47:34 +0530

    com

--- DSA/dsa/recursion/recursion.cpp
+++ DSA/dsa/recursion/recursion.cpp
@@ -1,17 +1,34 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-int print(int n){
-    if(n==0){   
-        return n;
-    }
-    return n + print(n-1);
-}
+// sum of numbers 1 to n
 
-//learn normal recursion, backtracking
+// int printsum(int n){
+//     if(n==0){   
+//         return n;
+//     }
+//     return n + print(n-1);
+// }
+
+//reverse an array
+
+void rev(int ar[],int start, int end){
+    if(start>=end){
+        for(int i=0; i<4; i++){
+            cout<<ar[i]<<endl;
+        }
+        return;
+    }
+    swap(ar[start],ar[end]);
+    rev(ar, start+1, end-1);
+    
+}
 
 int main(){
-    int n; cin>>n;
-    cout<<print(n);
+    
+    int arr[]{1,2,3,4};
+    rev(arr,0,3);
+
     return 0;
 }
+

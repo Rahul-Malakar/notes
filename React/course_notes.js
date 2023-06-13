34bb053db78c9c2305495cb86a49b535ef38ed67
Rahul <malakarahul111@gmail.com>
2023-06-13 12:17:23 +0530

    com

---
+++ React/course_notes.js
@@ -0,0 +1,8 @@
+// React defines what a component is and how multiple components work together
+
+// ReactDOM knows how to get a component to show up in the browser or mobile devices
+
+// jsx -> babel -> browser-runable-code
+// browser doesn't understand jsx so we need babel
+// why don't we write browser readable code is because of simplicity
+
--- React/jsx/src/index.js
+++ React/jsx/src/index.js
@@ -1,17 +1,19 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+// import react and react dom libraries
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+import React from "react";
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+import ReactDOM from "react-dom/client";
+
+// get a reference to the div with id root
+
+const el = document.getElementById('root');
+ 
+// tell react to take controll of that element
+const root = ReactDOM.createRoot(el);
+// create a component
+
+function App(){
+    return <div><h1>Hello</h1></div>;
+}
+// show the component on the screen
+root.render(<App />);
